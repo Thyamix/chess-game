ea5e2c8c9df6b21ed6f55b6d9adf4b41ea5d75b5
Thyamix <contact@thyamix.com>
2026-05-03 19:36:35 +0200

    feat: basic board and pieces

--- go.mod
+++ go.mod
@@ -2,12 +2,16 @@ module github.com/thyamix/chess-game
 
 go 1.26.2
 
-require github.com/hajimehoshi/ebiten v1.12.13
+require (
+	github.com/hajimehoshi/ebiten/v2 v2.9.9
+	github.com/thyamix/go-chess v0.0.0-20260503110319-0964cb6e56d5
+)
 
 require (
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200707082815-5321531c36a2 // indirect
-	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
-	golang.org/x/image v0.0.0-20200801110659-972c09e46d76 // indirect
-	golang.org/x/mobile v0.0.0-20210208171126-f462b3930c8f // indirect
-	golang.org/x/sys v0.0.0-20200918174421-af09f7315aff // indirect
+	github.com/ebitengine/gomobile v0.0.0-20250923094054-ea854a63cce1 // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.9.0 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,30 +1,134 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
-	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	chess "github.com/thyamix/go-chess"
 )
 
-type Game struct{}
+const GRIDSIZE int = 30
 
-func (g *Game) Update(*ebiten.Image) error {
+type Game struct {
+	board    chess.Board
+	selected *[2]int
+}
+
+func NewGame() *Game {
+	return &Game{
+		board: chess.NewBoard(),
+	}
+}
+
+func (g *Game) Update() error {
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButton0) {
+		if g.selected == nil {
+			x, y := ebiten.CursorPosition()
+			x = (8*GRIDSIZE - x) / GRIDSIZE
+			y = (8*GRIDSIZE - y) / GRIDSIZE
+			piece, _ := g.board.GetPiece(x, y)
+			if piece.Type() != chess.EMPTY {
+				g.selected = &[2]int{x, y}
+			}
+		} else {
+			g.selected = nil
+		}
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	DrawBoard(screen)
+	for x := range 8 {
+		for y := range 8 {
+			piece, err := g.board.GetPiece(x, y)
+			if err != nil || piece.Type() == chess.EMPTY {
+				continue
+			}
+			if g.selected != nil && g.selected[0] == x && g.selected[1] == y {
+				xpos, ypos := ebiten.CursorPosition()
+
+				DrawPiece(screen, xpos, ypos, false, piece)
+			} else {
+				DrawPiece(screen, x, y, true, piece)
+			}
+		}
+	}
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+func DrawPiece(screen *ebiten.Image, x int, y int, onGrid bool, piece chess.Piece) {
+	var img *ebiten.Image
+	switch piece.Type() {
+	case chess.PAWN:
+		if piece.IsBlack() {
+			img, _, _ = ebitenutil.NewImageFromFile("assets/pieces/black_pawn.png")
+		} else {
+			img, _, _ = ebitenutil.NewImageFromFile("assets/pieces/white_pawn.png")
+		}
+	case chess.BISHOP:
+		if piece.IsBlack() {
+			img, _, _ = ebitenutil.NewImageFromFile("assets/pieces/black_bishop.png")
+		} else {
+			img, _, _ = ebitenutil.NewImageFromFile("assets/pieces/white_bishop.png")
+		}
+	case chess.KING:
+		if piece.IsBlack() {
+			img, _, _ = ebitenutil.NewImageFromFile("assets/pieces/black_king.png")
+		} else {
+			img, _, _ = ebitenutil.NewImageFromFile("assets/pieces/white_king.png")
+		}
+	case chess.KNIGHT:
+		if piece.IsBlack() {
+			img, _, _ = ebitenutil.NewImageFromFile("assets/pieces/black_knight.png")
+		} else {
+			img, _, _ = ebitenutil.NewImageFromFile("assets/pieces/white_knight.png")
+		}
+	case chess.QUEEN:
+		if piece.IsBlack() {
+			img, _, _ = ebitenutil.NewImageFromFile("assets/pieces/black_queen.png")
+		} else {
+			img, _, _ = ebitenutil.NewImageFromFile("assets/pieces/white_queen.png")
+		}
+	case chess.ROOK:
+		if piece.IsBlack() {
+			img, _, _ = ebitenutil.NewImageFromFile("assets/pieces/black_rook.png")
+		} else {
+			img, _, _ = ebitenutil.NewImageFromFile("assets/pieces/white_rook.png")
+		}
+	}
+	geo := ebiten.GeoM{}
+	if onGrid {
+		geo.Translate(float64((7*GRIDSIZE)-(GRIDSIZE*x)+GRIDSIZE/4), float64((7*GRIDSIZE)-(GRIDSIZE*y)+GRIDSIZE/4))
+	} else {
+		geo.Translate(float64(x-GRIDSIZE/4), float64(y-GRIDSIZE/4))
+	}
+	screen.DrawImage(img, &ebiten.DrawImageOptions{GeoM: geo})
+}
+
+func DrawBoard(screen *ebiten.Image) {
+	for x := range 8 {
+		for y := range 8 {
+			img := ebiten.NewImage(GRIDSIZE, GRIDSIZE)
+			img.Fill(color.RGBA{255, 200, uint8((255) * ((y + x) % 2)), 255})
+			geo := ebiten.GeoM{}
+			geo.Translate(float64(GRIDSIZE*x), float64(GRIDSIZE*y))
+			screen.DrawImage(img, &ebiten.DrawImageOptions{GeoM: geo})
+		}
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return 8 * GRIDSIZE, 8 * GRIDSIZE
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+
+	ebiten.SetWindowSize(800, 800)
+	ebiten.SetWindowTitle("Chess")
+	if err := ebiten.RunGame(NewGame()); err != nil {
 		log.Fatal(err)
 	}
 }
